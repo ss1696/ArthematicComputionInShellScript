f570e59c10d9518e864bc941a6ffbe64258dfb13
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 17:50:00 +0530

    Storing results in Dictionary

--- ArthematicComputation.sh
+++ ArthematicComputation.sh
@@ -8,3 +8,10 @@ result2=$(($a*$b+$c))
 result3=$(($c+$a/$b))
 result4=$(($a%$b+$c))
 
+#storing above result in dictionary
+declare -a comptn
+comptn[1]=$result1
+comptn[2]=$result2
+comptn[3]=$result3
+comptn[4]=$result4
+
